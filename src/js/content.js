90cf724e7cf08cd7e0933a5b4edc9363c6eeda24
Moe Amaya <moeamaya@gmail.com>
2022-11-13 21:19:31 -0800

    Arch working

--- src/js/content.js
+++ src/js/content.js
@@ -1,4 +1,5 @@
 import bling from "./helpers/bling";
+import interact from 'interactjs'
 
 bling();
 let plants = [];
@@ -6,19 +7,24 @@ let plants = [];
 
 const init = () => {
   const height = document.body.scrollHeight;
+  const width = window.innerWidth;
+
   const sticky = $('.sticky')[0];
   const stickyRect = sticky.getBoundingClientRect()
   const baseline = stickyRect.top - window.innerHeight + stickyRect.height;
   const delta = height - baseline;
-  console.log(delta);
+  const restriction = $('.plants')[0];
 
   setPlants(baseline, delta);
+  setInteractables(width);
+  setDrag(restriction);
 
   addEventListeners();
 };
 
 const setPlants = (baseline, delta) => {
   const plantNodes = $('.plant');
+
   plantNodes.forEach((node, index) => {
     const gate = delta / (plantNodes.length + 3);
     const obj = {
@@ -27,6 +33,51 @@ const setPlants = (baseline, delta) => {
     }
     plants.push(obj);
   });
+};
+
+const setInteractables = (width) => {
+  const innerNodes = $('.inner');
+  const gap = (width - (innerNodes.length * 200)) / (innerNodes.length - 1);
+
+  innerNodes.forEach((node, index) => {
+    const plantImageNode = $('.plant-image')[0].cloneNode(true);
+    node.appendChild(plantImageNode);
+
+    const space = (200 * index) + (gap * index);
+    node.style.transform = 'translate(' + space + 'px, 0px)'
+    node.setAttribute('data-x', space);
+  });
+}
+
+const setDrag = (restriction) => {
+  const plant = interact('.inner');
+
+  plant.draggable({
+    inertia: true,
+    modifiers: [
+      interact.modifiers.restrictRect({
+        restriction: restriction,
+        endOnly: true
+      })
+    ],
+    listeners: {
+      move: dragMoveListener
+    }
+  })
+}
+
+const dragMoveListener = (event) => {
+  var target = event.target
+  // keep the dragged position in the data-x/data-y attributes
+  var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx
+  var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy
+
+  // translate the element
+  target.style.transform = 'translate(' + x + 'px, ' + y + 'px)'
+
+  // update the posiion attributes
+  target.setAttribute('data-x', x)
+  target.setAttribute('data-y', y)
 }
 
 const updateScroll = () => {
